42c56eb9b8b5c9502ddac65c8d2e83677d4e38cf
Allan Pedin <allan.pedin.17@cnu.edu>
2020-03-28 05:45:58 -0400

    added sleep

--- print_ts.cpp
+++ print_ts.cpp
@@ -9,27 +9,37 @@
 std::mutex mtx;
 bool canceled = false;
 void PRINT1(std::string &txt) {
-    std::lock_guard<std::mutex> guard(mtx);
-    std:: cout <<txt<<"\n";
+    if(!canceled){
+        std::lock_guard<std::mutex> guard(mtx);
+        std:: cout <<txt<<"\n";
+    }
 }
 
 void PRINT2(std::string &txt, std::string &txt1) {
-     std::lock_guard<std::mutex> guard(mtx);
-     std:: cout <<txt<<txt1<<"\n";
+    if(!canceled) {
+        std::lock_guard<std::mutex> guard(mtx);
+        std::cout << txt << txt1 << "\n";
+    }
 }
 
 void PRINT3(std::string &txt, std::string &txt1, std::string &txt2) {
-     std::lock_guard<std::mutex> guard(mtx);
-     std:: cout <<txt<<txt1<<txt2<<"\n";
+    if(!canceled) {
+        std::lock_guard<std::mutex> guard(mtx);
+        std::cout << txt << txt1 << txt2 << "\n";
+    }
 }
 
 void PRINT4(std::string &txt, std::string &txt1, std::string &txt2, std::string &txt3) {
-     std::lock_guard<std::mutex> guard(mtx);
-     std:: cout <<txt<<txt1<<txt2 <<txt3<<"\n";
+    if(!canceled) {
+        std::lock_guard<std::mutex> guard(mtx);
+        std::cout << txt << txt1 << txt2 << txt3 << "\n";
+    }
 }
 
 void PRINT5(std::string &txt, std::string &txt1, std::string &txt2, std::string &txt3, std::string &txt4) {
-     std::lock_guard<std::mutex> guard(mtx);
-     std:: cout <<txt<<txt1<<txt2<<txt3<<txt4<<"\n";
+    if(!canceled) {
+        std::lock_guard<std::mutex> guard(mtx);
+        std::cout << txt << txt1 << txt2 << txt3 << txt4 << "\n";
+    }
 }
 
--- tester.cpp
+++ tester.cpp
@@ -3,6 +3,8 @@
 //
 #include <vector>
 #include <thread>
+#include <time.h>
+#include <chrono>
 #include "tester.h"
 #include "./print_ts.h"
 
@@ -17,35 +19,39 @@ void startThreads(std::string s, int numThreads, WHICH_PRINT wp, int numTimesToP
         case P1:
             for(int i =0;i<numThreads;i++){
                 threads.push_back(thread(PRINT1,sRef));
+                this_thread::sleep_for(chrono::milliseconds(millisecond_delay));
             }
             break;
         case P2:
             for(int i =0;i<numThreads;i++) {
                 threads.push_back(thread(PRINT2,sRef,sRef));
+                this_thread::sleep_for(chrono::milliseconds(millisecond_delay));
             }
             break;
         case P3:
             for(int i =0;i<numThreads;i++) {
                 threads.push_back(thread(PRINT3,sRef,sRef,sRef));
+                this_thread::sleep_for(chrono::milliseconds(millisecond_delay));
             }
             break;
         case P4:
             for(int i =0;i<numThreads;i++) {
                 threads.push_back(thread(PRINT4,sRef,sRef,sRef,sRef));
+                this_thread::sleep_for(chrono::milliseconds(millisecond_delay));
             }
             break;
         case P5:
             for(int i =0;i<numThreads;i++) {
                 threads.push_back(thread(PRINT5,sRef,sRef,sRef,sRef,sRef));
+                this_thread::sleep_for(chrono::milliseconds(millisecond_delay));
             }
             break;
     }
 }
 
 void setCancelThreads(bool bCancel) {
-    for(thread &t: threads){
-        t.
-    }}
+    canceled = bCancel
+}
 
 void joinThreads() {
     for(thread &t: threads){
