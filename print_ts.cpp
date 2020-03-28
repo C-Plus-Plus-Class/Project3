3d99470cc0e266e422650c9de3b9838ab1c2e4cf
Allan Pedin <allan.pedin.17@cnu.edu>
2020-03-28 04:19:10 -0400

    wrote some print functions

--- print_ts.cpp
+++ print_ts.cpp
@@ -1,4 +1,35 @@
-//
+ //
 // Created by allan on 3/28/20.
 //
+
+#include <iostream>
+#include <mutex>
+#include "print_ts.h"
+
+ std::mutex mtx;
+ void PRINT1(std::string &txt) {
+     std::lock_guard<std::mutex> guard(mtx);
+     std:: cout <<&txt<<"\n";
+ }
+
+ void PRINT2(std::string &txt, std::string &txt1) {
+     std::lock_guard<std::mutex> guard(mtx);
+     std:: cout <<&txt<<&txt1<<"\n";
+ }
+
+ void PRINT3(std::string &txt, std::string &txt1, std::string &txt2) {
+     std::lock_guard<std::mutex> guard(mtx);
+     std:: cout <<&txt<<&txt1<<&txt2<<"\n";
+     mtx.unlock();
+ }
+
+ void PRINT4(std::string &txt, std::string &txt1, std::string &txt2, std::string &txt3) {
+     std::lock_guard<std::mutex> guard(mtx);
+     std:: cout <<&txt<<&txt1<<&txt2 <<&txt3<<"\n";
+ }
+
+ void PRINT5(std::string &txt, std::string &txt1, std::string &txt2, std::string &txt3, std::string &txt4) {
+     std::lock_guard<std::mutex> guard(mtx);
+     std:: cout <<&txt<<&txt1<<&txt2<<&txt3<<&txt4<<"\n";
+ }
 
--- tester.cpp
+++ tester.cpp
@@ -2,3 +2,17 @@
 // Created by allan on 3/28/20.
 //
 
+#include "tester.h"
+
+void startThreads(std::string s, int numThreads, WHICH_PRINT wp, int numTimesToPrint, int millisecond_delay) {
+
+}
+
+void setCancelThreads(bool bCancel) {
+
+}
+
+void joinThreads() {
+
+}
+
