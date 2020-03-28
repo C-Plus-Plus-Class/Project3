e54e661db239301bfb6f5e4ceb06db1e4401b179
Allan Pedin <allan.pedin.17@cnu.edu>
2020-03-28 06:02:12 -0400

    works in theory now

--- print_ts.cpp
+++ print_ts.cpp
@@ -7,39 +7,28 @@
 #include "print_ts.h"
 
 std::mutex mtx;
-bool canceled = false;
 void PRINT1(std::string &txt) {
-    if(!canceled){
-        std::lock_guard<std::mutex> guard(mtx);
-        std:: cout <<txt<<"\n";
-    }
+    std::lock_guard<std::mutex> guard(mtx);
+    std:: cout <<txt<<"\n";
 }
 
 void PRINT2(std::string &txt, std::string &txt1) {
-    if(!canceled) {
-        std::lock_guard<std::mutex> guard(mtx);
-        std::cout << txt << txt1 << "\n";
-    }
+    std::lock_guard<std::mutex> guard(mtx);
+    std::cout << txt << txt1 << "\n";
 }
 
 void PRINT3(std::string &txt, std::string &txt1, std::string &txt2) {
-    if(!canceled) {
-        std::lock_guard<std::mutex> guard(mtx);
-        std::cout << txt << txt1 << txt2 << "\n";
-    }
+    std::lock_guard<std::mutex> guard(mtx);
+    std::cout << txt << txt1 << txt2 << "\n";
 }
 
 void PRINT4(std::string &txt, std::string &txt1, std::string &txt2, std::string &txt3) {
-    if(!canceled) {
-        std::lock_guard<std::mutex> guard(mtx);
-        std::cout << txt << txt1 << txt2 << txt3 << "\n";
-    }
+    std::lock_guard<std::mutex> guard(mtx);
+    std::cout << txt << txt1 << txt2 << txt3 << "\n";
 }
 
 void PRINT5(std::string &txt, std::string &txt1, std::string &txt2, std::string &txt3, std::string &txt4) {
-    if(!canceled) {
-        std::lock_guard<std::mutex> guard(mtx);
-        std::cout << txt << txt1 << txt2 << txt3 << txt4 << "\n";
-    }
+    std::lock_guard<std::mutex> guard(mtx);
+    std::cout << txt << txt1 << txt2 << txt3 << txt4 << "\n";
 }
 
--- tester.cpp
+++ tester.cpp
@@ -12,40 +12,44 @@
 using namespace std;
 
 vector<thread> threads;
-
-void startThreads(std::string s, int numThreads, WHICH_PRINT wp, int numTimesToPrint, int millisecond_delay) {
-    auto sRef = ref(s);
-    switch(wp){
+bool canceled = false;
+void cancelableFunction(std::string s, WHICH_PRINT wp, int numTimesToPrint, int millisecond_delay){
+    switch(wp){//could do this with switch and set a function variable to a specific function and have only 1 for loop, but this was easier and we already have 5 redundant print functions so eh
         case P1:
-            for(int i =0;i<numThreads;i++){
-                threads.push_back(thread(PRINT1,sRef));
+            for(int i = 0; i<numTimesToPrint && !canceled; i++){
+                PRINT1(s);
                 this_thread::sleep_for(chrono::milliseconds(millisecond_delay));
             }
             break;
         case P2:
-            for(int i =0;i<numThreads;i++) {
-                threads.push_back(thread(PRINT2,sRef,sRef));
+            for(int i = 0; i<numTimesToPrint && !canceled; i++){
+                PRINT2(s,s);
                 this_thread::sleep_for(chrono::milliseconds(millisecond_delay));
             }
             break;
         case P3:
-            for(int i =0;i<numThreads;i++) {
-                threads.push_back(thread(PRINT3,sRef,sRef,sRef));
-                this_thread::sleep_for(chrono::milliseconds(millisecond_delay));
+            for(int i = 0; i<numTimesToPrint && !canceled; i++){
+                PRINT3(s,s,s);
+                this_thread::sleep_for(chrono::milliseconds(millisecond_delay))
             }
             break;
         case P4:
-            for(int i =0;i<numThreads;i++) {
-                threads.push_back(thread(PRINT4,sRef,sRef,sRef,sRef));
+            for(int i = 0; i<numTimesToPrint && !canceled; i++){
+                PRINT4(s,s,s,s);
                 this_thread::sleep_for(chrono::milliseconds(millisecond_delay));
             }
             break;
         case P5:
-            for(int i =0;i<numThreads;i++) {
-                threads.push_back(thread(PRINT5,sRef,sRef,sRef,sRef,sRef));
+            for(int i = 0; i<numTimesToPrint && !canceled; i++){
+                PRINT5(s,s,s,s,s);
                 this_thread::sleep_for(chrono::milliseconds(millisecond_delay));
             }
             break;
+    }
+}
+void startThreads(std::string s, int numThreads, WHICH_PRINT wp, int numTimesToPrint, int millisecond_delay) {
+    for(int i =0;i<numThreads;i++){
+        threads.push_back(thread(cancelableFunction,s,wp,numTimesToPrint,millisecond_delay));
     }
 }
 
